29c0b0b6ae600ca0ab018495d1ec204dde67f4bf
mikitka266 <107499465+mikitka266@users.noreply.github.com>
2023-07-17 13:03:06 +0300

    Add files via upload

---
+++ argparse.py
@@ -0,0 +1,17 @@
+import argparse
+
+def quadratic_equations(a,b,c):
+    d = b**2 - 4*a*c
+    if d > 0:
+        return (-b + d**0.5) / (2*a), (-b -d ** 0.5) / (2*a)
+    if d == 0:
+        return -b / (2*a)
+    return None
+
+if __name__ == '__main__':
+    parser = argparse.Argumentparser(description = 'Solving quadratic equations')
+    parser.add_argument('-a', metevar='a', type=float, help = 'enter a for ax^2+bx+c = 0', default=1)
+    parser.add_argument('-b', metevar='b', type=float, help='enter b for ax^2+bx+c = 0', default=0)
+    parser.add_argument('-c', metevar='c', type=float, help='enter c for ax^2+bx+c = 0', default=0)
+    args = parser.parse_args()
+    print(quadratic_equations(args.a, args.b, args.c))
