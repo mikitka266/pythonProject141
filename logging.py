62c868535348f93ae4063c7d916ee8c527bba020
mikitka266 <107499465+mikitka266@users.noreply.github.com>
2023-09-14 18:41:16 +0300

    Delete logging.py

--- logging.py
+++
@@ -1,8 +0,0 @@
-import logging
-
-logging.basicConfig(level=logging.NOTSET)
-logging.debug('A lot of information. Change a lot of prints')
-logging.info('Not a lot information about working of code')
-logging.warning('Warning! you have a variety of dangerous')
-logging.error('We catch the error')
-logging.critical('It is over')
